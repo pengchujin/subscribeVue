f9cfe1450e674236551607652dba0ccea0073e20
pengchujin <pengchujin@blackbeanssoft.com>
2018-11-27 01:46:43 +0800

    add copy to default subscribe

--- pages/user/subscribe.vue
+++ pages/user/subscribe.vue
@@ -1,20 +1,51 @@
 <template>
-  <div><p>>默认订阅地址</p>
-  <p>http://test.taolu.cloud:3001/allnodes/{{links}}</p></div>
+  <div>
+    <div class="header"><p>>SSR默认订阅地址</p>
+    </div>
+   <div class="content">
+      <div><p>{{links}}</p></div>
+      <div class="copy"><el-button v-clipboard:copy="links"
+      v-clipboard:success="onCopy"
+      v-clipboard:error="onError" type="primary" round>复制链接</el-button></div>
+   </div>
+  </div>
 </template>
 
 
 <script>
+
   export default {
     data(){
       return {
         link: ''
       }
     },
+    methods: {
+      onCopy: function (e) {
+      this.$message('复制成功');
+    },
+    onError: function (e) {
+      this.$message('复制失败 = = ');
+    }
+    },
     computed:{
       links() {
-        return this.$store.state.user.id
+        return 'http://test.taolu.cloud:3001/allnodes/' + this.$store.state.user.id
       }
     }
   }
 </script>
+
+<style>
+  .header {
+    font-size: 30px;
+    padding: 30px;
+  }
+  .content {
+    font-size: 20px;
+    display:inline;
+  }
+  .copy{
+    padding-top: 16px;
+  }
+</style>
--- plugins/element-ui.js
+++ plugins/element-ui.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Element from 'element-ui/lib/element-ui.common'
 import locale from 'element-ui/lib/locale/lang/en'
-
+import VueClipboard from 'vue-clipboard2'
+Vue.use(VueClipboard)
 export default () => {
   Vue.use(Element, { locale })
 }
