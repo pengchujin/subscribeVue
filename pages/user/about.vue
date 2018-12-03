dd0fa383faed18bb9c741c2021bada1e3ef78a2f
pengchujin <pengchujin@blackbeanssoft.com>
2018-12-04 00:44:11 +0800

    add share

--- pages/user/about.vue
+++ pages/user/about.vue
@@ -1,4 +1,18 @@
 <template>
-  <p>123</p>
+  <div class="about" align="left">
+    <p >>BE ME</p>
+    <p>>SEB'S CLUB</p>
+    <p>>HERE'S TO THE FOOLS WHO DREAM</p>
+    <p>>CRAZY AS THEY MAY SEEM</p>
+    <p>>HERE'S TO THE HEARTS THAT BREAK</p>
+    <p>>HERE'S TO THE MESS WE MAKE</p>
+  </div>
 </template>
 
+<style>
+  .about{
+    padding-left: 300px;
+    padding-top: 80px;
+    font-size: 70px;
+  }
+</style>
--- pages/user/share.vue
+++ pages/user/share.vue
@@ -1,3 +1,78 @@
 <template>
-  <p>Share</p>
+  <div>
+    <div class="header"><p>>SSR免费共享订阅地址</p>
+    </div>
+   <div class="content">
+      <div><p>{{links}}</p></div>
+      <div class="copy"><el-button v-clipboard:copy="links"
+      v-clipboard:success="onCopy"
+      v-clipboard:error="onError" type="primary" round>复制链接</el-button>
+      <el-button @click="iOS" type="primary" round>
+        iOS快捷键</el-button></div>
+   </div>
+   <br>
+   <p>shadowRocket 二维码</p>
+   <qrcode class="qrcode" :value="msg" :options="{ size: 350 }"></qrcode>
+  </div>
+ 
 </template>
+
+
+<script>
+import VueQrcode from '@xkeshi/vue-qrcode';
+  export default {
+    data(){
+      return {
+        api: 'https://share.sebs.club',
+        msg: ''
+      }
+    },
+    methods: {
+      onCopy: function (e) {
+      this.$message('复制成功');
+    },
+    onError: function (e) {
+      this.$message('复制失败 = = ');
+    },
+     iOS() {
+       window.location.href = this.msg
+     }
+    },
+    async created() {
+      let url = await this.api
+      this.msg = 'sub://' + Buffer.from(url).toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/\=+$/, '') + '#'
+    },
+    computed:{
+      links() {
+        return this.api
+      }
+    },
+    comments: {
+  
+    }
+  }
+</script>
+
+<style>
+  .header {
+    font-size: 30px;
+    padding: 30px;
+  }
+  .content {
+    text-decoration: none;
+    font-size: 20px;
+    display:inline;
+  }
+  .copy{
+    padding-top: 16px;
+  }
+  .el-button{
+    text-decoration: none;
+  }
+  .a{
+    text-decoration: none;
+  }
+  .qrcode {
+    padding-top: 8px;
+  }
+</style>
